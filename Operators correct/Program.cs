de0943e8419c24ab6525d57df1e7731f15d6a490
unknown <Dunyafm@code.edu.az>
2021-12-05 14:37:23 -0800

    Operators correct

---
+++ Operators correct/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operators
+{
+
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("birinci ededi daxil edin : ");
+        int fNum = Convert.ToInt32(Console.ReadLine());
+        Console.Write("ikinci ededi daxil edin : ");
+        int sNum = Convert.ToInt32(Console.ReadLine());
+        Console.Write("operator daxil edin : ");
+        string optr = Console.ReadLine();
+
+        switch (optr)
+        {
+            case "+":
+                Console.WriteLine(fNum + sNum);
+                break;
+            case "-":
+                Console.WriteLine(fNum - sNum);
+                break;
+            case "*":
+                Console.WriteLine(fNum * sNum);
+                break;
+            case "/":
+                Console.WriteLine(fNum / sNum);
+                break;
+            default:
+                Console.WriteLine("incorrect operator");
+                break;
+
+        }
+    }
+}
+
+
+
+
+
